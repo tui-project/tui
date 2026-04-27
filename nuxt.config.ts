db32ff2cfedfec17452f87d037b80800d3b5c6b2
tui-project <vague.battle-1j@icloud.com>
2026-04-27 15:44:18 +1200

    initial project setup with Nuxt

---
+++ nuxt.config.ts
@@ -0,0 +1,12 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    compatibilityDate: '2025-07-15',
+    devtools: { enabled: true },
+
+    typescript: {
+        typeCheck: true,
+    },
+
+    modules: ['@nuxt/eslint', '@nuxt/ui', '@nuxt/test-utils/module', '@nuxt/image', '@nuxt/image'],
+    css: ['~/assets/css/main.css'],
+})
---
+++ prettier.config.mjs
@@ -0,0 +1,13 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+    trailingComma: 'es5',
+    printWidth: 180,
+    tabWidth: 4,
+    semi: false,
+    singleQuote: true,
+}
+
+export default config
